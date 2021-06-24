f288e759109a47887b9b74d69ac2bcf1cc8ad18b
angesco <angeloo.esc@gmail.com>
2021-06-23 22:07:15 -0300

    pagination

--- client/src/components/Games/Games.js
+++ client/src/components/Games/Games.js
@@ -7,7 +7,7 @@ import { addGameFavorite } from '../../actions/index.js'
 export function Game(props){
 	return(
 		<div className="card">
-             <button alt="Add to favorite" className="favVg material-icons" onClick={() => props.addGameFavorite({title: props.name, id: props.id, img: props.img, genres: props.genres})}>star_outline</button>
+             <button alt="Add to favorite" className="favVg material-icons" onClick={() => props.addGameFavorite({title: props.name, id: props.id, img: props.img, genres: props.genres})}>star</button>
                 <Link to={`/home/details/${props.id}`} className="titleLink">
             <p className="title">
                 {props.name}                
--- client/src/components/Videogames/Videogame.js
+++ client/src/components/Videogames/Videogame.js
@@ -1,22 +1,25 @@
 import React, { useState, useEffect} from 'react';
 import { connect } from 'react-redux';
-
 import Games from '../Games/Games.js';
 import { getGame, getAllGames, addGameFavorite} from '../../actions/index.js'
 import './Videogame.css'
 import {Link} from 'react-router-dom';
 
-
-
 export function Videogame(props){
 
-	const [numeroPagina, setNumeroPagina] = useState(1);
+	const [numeroPagina, setNumeroPagina] = useState(1); 
 
 	const grupo = 15;
 	const conteoFinal = numeroPagina * grupo;
 	const conteoInicial = conteoFinal - grupo;
    let vgames;
+   const totalPages = Math.ceil(props.videogames.length / 15);
+   const allPages = [];
+   for(let i = 1; i <= totalPages; i++){
+     allPages.push(i)
+   }
 
+   console.log(allPages)
 	props.filterBy === "All"
     ? (vgames = props.videogames.slice(conteoInicial, conteoFinal))
     : (vgames = props.filteredVideogames.slice(conteoInicial, conteoFinal));
@@ -31,16 +34,6 @@ export function Videogame(props){
 
     const videogameData = vgames.map(game => {
 
-        // let generos = [];
-
-		// if(game.genres){
-        // for(let i = 0; i < game.genres.length; i++) {
-        //    generos.push(game.genres[i].name)
-        //   } }
-        
-    
-
-
        return (<div className="divGame" key={game.id}>
             <Games
             name={game.name}
@@ -54,19 +47,53 @@ export function Videogame(props){
         </div>)
         })
 
+		const pageData = allPages.map(p => {
+            if(p === allPages[0] || p === allPages[allPages.length - 1]){
+
+			}
+			else if (p === numeroPagina - 1 || p === numeroPagina - 2 || p === numeroPagina + 1 || p === numeroPagina + 2){
+			return (
+				<Link to={`/home?pag=${p}`}>
+				<button className="currentPage" onClick={() => setNumeroPagina(p)}>{p}</button>
+				</Link>				
+			)} 
+			else if ( p === numeroPagina ) {
+				return (
+					<Link to={`/home?pag=${p}`}>
+					<button className="current" onClick={() => setNumeroPagina(p)}>{p}</button>
+					</Link>				
+				)	
+			}
+
+			 })
+
 	return (
 	<div className="allHome">
 
 {/*---------------PAGINADO BOTONES------------------*/}
-
 <div className="paginationBtns">
-                 <Link to={`/home?pag=${numeroPagina - 1}`}>
+                 { allPages[0] === numeroPagina? null : 
+					 <Link to={`/home?pag=${numeroPagina - 1}`}>
 					<button className="btnBack material-icons" onClick={() => {if(numeroPagina > 1) setNumeroPagina(numeroPagina - 1)}}>arrow_left</button>
+			     </Link>}
+				 <Link to={`/home?pag=${allPages[0]}`}>
+			         <button className={numeroPagina === allPages[0]? "current" : "currentPage"} onClick={() => setNumeroPagina(allPages[0])}>{allPages[0]}</button>
 			     </Link>
-				    <button className="currentPage">{numeroPagina}</button>
-				<Link to={`/home?pag=${numeroPagina + 1}`}>
+			    {
+					(numeroPagina -2) - allPages[0] >= 2? <button className="range">...</button> : null
+				}
+				 {pageData}
+				{
+					allPages[allPages.length - 1] - (numeroPagina + 2) >= 2? <button className="range">...</button> : null
+				}
+				 <Link to={`/home?pag=${allPages[allPages.length - 1]}`}>
+			<button className={numeroPagina === allPages[allPages.length - 1]? "current" : "currentPage"} onClick={() => setNumeroPagina(allPages[allPages.length - 1])}>{allPages[allPages.length - 1]}</button>
+			</Link>
+			    {
+				     allPages[allPages.length - 1] === numeroPagina? null :
+				 <Link to={`/home?pag=${numeroPagina + 1}`}>
 			        <button className="btnNext material-icons" onClick={() => setNumeroPagina(numeroPagina + 1)}>arrow_right</button>
-			    </Link>
+			    </Link>}
 		        </div>
 		
 
@@ -81,13 +108,28 @@ export function Videogame(props){
 {/*---------------PAGINADO BOTONES------------------*/}
 
 <div className="paginationBtns">
-                 <Link to={`/home?pag=${numeroPagina - 1}`}>
+                 { allPages[0] === numeroPagina? null : 
+					 <Link to={`/home?pag=${numeroPagina - 1}`}>
 					<button className="btnBack material-icons" onClick={() => {if(numeroPagina > 1) setNumeroPagina(numeroPagina - 1)}}>arrow_left</button>
+			     </Link>}
+				 <Link to={`/home?pag=${allPages[0]}`}>
+			         <button className={numeroPagina === allPages[0]? "current" : "currentPage"} onClick={() => setNumeroPagina(allPages[0])}>{allPages[0]}</button>
 			     </Link>
-				    <button className="currentPage">{numeroPagina}</button>
-				<Link to={`/home?pag=${numeroPagina + 1}`}>
+			    {
+					(numeroPagina -2) - allPages[0] >= 2? <button className="range">...</button> : null
+				}
+				 {pageData}
+				{
+					allPages[allPages.length - 1] - (numeroPagina + 2) >= 2? <button className="range">...</button> : null
+				}
+				 <Link to={`/home?pag=${allPages[allPages.length - 1]}`}>
+			<button className={numeroPagina === allPages[allPages.length - 1]? "current" : "currentPage"} onClick={() => setNumeroPagina(allPages[allPages.length - 1])}>{allPages[allPages.length - 1]}</button>
+			</Link>
+			    {
+				     allPages[allPages.length - 1] === numeroPagina? null :
+				 <Link to={`/home?pag=${numeroPagina + 1}`}>
 			        <button className="btnNext material-icons" onClick={() => setNumeroPagina(numeroPagina + 1)}>arrow_right</button>
-			    </Link>
+			    </Link>}
 		        </div>
 			</div>
 		</div>
